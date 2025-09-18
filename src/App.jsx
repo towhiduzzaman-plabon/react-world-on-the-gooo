8f8cbc628976e8f4b5421e10116dd577ae55a6bb
Towhiduzzaman Plabon <plabonofficial24@gmail.com>
2025-09-18 22:19:14 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Countries from './Countries.jsx/Countries/Countries'
+
+function App() {
+
+  return (
+    <>
+    
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
---
+++ src/Countries.jsx/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>In the countries</h1>
+        </div>
+    );
+};
+
+export default Countries;
